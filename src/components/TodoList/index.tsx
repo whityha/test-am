237c08fce6bfaf40c3d70af2351920ba5eb5ab11
Pavel <2042_2042@mail.ru>
2023-09-05 11:55:45 +0300

    feat: init vite/react project and add dependencies

---
+++ src/components/TodoList/index.tsx
@@ -0,0 +1,5 @@
+export const TodoList = () => {
+  return (
+    <div>TodoList</div>
+  )
+}
---
+++ src/components/app/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import { TodoList } from '../TodoList'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <TodoList />
+  )
+}
+
+export default App
